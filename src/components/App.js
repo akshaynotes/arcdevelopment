5d00d0228a1bace7dca49b4f5a4d91cb0db09bf8
Akshay <akshayware1207@gmail.com>
2021-05-02 15:58:21 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import { ThemeProvider } from "@material-ui/styles";
+import Header from "./ui/Header";
+import theme from './ui/Theme';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+        <BrowserRouter>
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={() => <div>Home</div>} />
+          <Route exact path="/services" component={() => <div>Services</div>} />
+          <Route exact path="/customsoftware" component={() => <div>Custom Software</div>} />
+          <Route exact path="/mobileapps" component={() => <div>Mobile Apps</div>} />
+          <Route exact path="/websites" component={() => <div>Websites</div>} />
+          <Route exact path="/revolution" component={() => <div>Revolution</div>} />
+          <Route exact path="/about" component={() => <div>About Us</div>} />
+          <Route exact path="/contact" component={() => <div>Contact Us</div>} />
+          <Route exact path="/estimate" component={() => <div>Free Estimate</div>} />
+        </Switch>
+        
+        </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
